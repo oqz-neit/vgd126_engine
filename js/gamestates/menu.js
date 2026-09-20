9929cd3146b737a4070876f256561256f80e920a
Osiris Quezada <oquezada1@email.neit.edu>
2026-09-19 21:47:59 -0400

    Menu & Button Set

--- js/gamestates/menu.js
+++ js/gamestates/menu.js
@@ -7,13 +7,16 @@ var startButton = new GameObject();
 startButton.width = 200;
 startButton.hitBoxWidth = 800;
 
+startButton.img.src = `images/Transmutate_button.png`;
+
+
 var menuBackground = new GameObject();
 
 /* =====================================================================
    ✏️ STUDENT EDIT ZONE: MENU BACKGROUND IMAGE
    Replace with your own JPG/PNG; keep aspect ratio close to canvas.
    ===================================================================== */
-menuBackground.img.src = "images/mrt.jpg";
+menuBackground.img.src = "images/main_menu_sprite.png";
 menuBackground.width = canvas.width;
 menuBackground.height = canvas.height;
 
@@ -21,11 +24,11 @@ gameStates[`menu`] = function () {
   // Hover & click
   if (startButton.overlap(mouse)) {
     if (mouse.pressed) { gameStates.changeState(`level1`); }
-    startButton.color = `yellow`;
+    startButton.img.src = `images/COMPLETE.png`
   } else {
-    startButton.color = `red`;
+    startButton.img.src = `images/Transmutate_button.png`;
   }
 
   menuBackground.drawStaticImage();
-  startButton.render();
+  startButton.drawStaticImage();
 };
